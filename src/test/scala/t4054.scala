dcf039e33940d18caa9b77998ee0659695688971
Konstantin Läufer <klaeufer@gmail.com>
2018-06-06 18:15:26 -0500

    updated test coverage report for t4054
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/t4054.scala
+++ src/test/scala/t4054.scala
@@ -1,21 +1,15 @@
 import org.junit.Test
 
+import scala.tools.testing.AssertUtil.assertSameElements
+
 class t4054 {
   @Test def scanLeftSimpleTest(): Unit = {
 
     val i0 = Iterator.from(1).map(n => n * n)
-    val it = IteratorMethods.scanLeft(i0)(0)(_ + _)
+    val it = IteratorMethods.scanLeft(i0)(0)(_ + _).take(5)
 
-    assert(it.next == 0)
-    assert(it.next == 1)
-    assert(it.next == 5)
-    assert(it.next == 14)
-    assert(it.next == 30)
-    assert(it.next == 55)
-    assert(it.next == 91)
-    assert(it.next == 140)
-    assert(it.next == 204)
-    assert(it.next == 285)
-    assert(it.next == 385)
+    // modified to ensure that the statement hasNext = false in scanLeft will be exercised
+
+    assertSameElements(List(0, 1, 5, 14, 30), it)
   }
 }
